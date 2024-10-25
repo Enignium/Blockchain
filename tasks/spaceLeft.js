5b005c27366208db942d9ce43f6fa1431ab3138b
Enignium <87068141+Enignium@users.noreply.github.com>
2024-10-25 12:11:58 +0200

    Update spaceLeft.js

--- tasks/spaceLeft.js
+++ tasks/spaceLeft.js
@@ -1,26 +1,12 @@
 const { task } = require("hardhat/config");
-
-task("spaceLeft", "le cose forse hanno senso")
-
+task("spaceLeft", "memoria")
   .setAction(async (taskArgs) => {
-
     const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-    
-    
     const FileStorage = await ethers.getContractFactory("FileStorage");
-
-     
-     const contract = FileStorage.attach(contractAddress);
-
+    const contract = FileStorage.attach(contractAddress);
     const account = process.env.ACCOUNT_ADDRESS;
-
-  
     const signer = await ethers.getSigner(account);
     const contractWithSigner = contract.connect(signer);
-
-
-   
     spaceleft = await contractWithSigner.getSpaceLeft()
     console.log(spaceleft);
-    
   });
