48a918a0b20401b9404ff712f5ad48d817b5b5cd
Enignium <87068141+Enignium@users.noreply.github.com>
2024-10-25 10:59:01 +0200

    Update upload.js

--- tasks/upload.js
+++ tasks/upload.js
@@ -9,26 +9,15 @@ task("upload", "testo")
     const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
   
     const FileStorage = await ethers.getContractFactory("FileStorage");
-
- 
     const contract = FileStorage.attach(contractAddress);
-
-    
     const account = process.env.ACCOUNT_ADDRESS;
-
-    
     const signer = await ethers.getSigner(account);
     const contractWithSigner = contract.connect(signer);
-
-
     const contentBytes = Buffer.from(taskArgs.content, 'utf-8');
-
     const spaceLeft = await contractWithSigner.getSpaceLeft();
     
     if(BigInt(spaceLeft) - BigInt(contentBytes.length)  > 0){
-
       const tx = await contractWithSigner.uploadFile(taskArgs.name, contentBytes);
-
       await tx.wait();
 
       console.log(`File caricato: ${taskArgs.name} con contenuto: ${taskArgs.content}`);
