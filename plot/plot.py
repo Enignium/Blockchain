8bfe9a5831470ce58a63d7af12e92fdbae5a306e
xAlekos <garufialessandro0@gmail.com>
2024-11-06 00:20:40 +0100

    ora si testa e si può plottare

---
+++ plot/plot.py
@@ -0,0 +1,21 @@
+import json
+import matplotlib.pyplot as plt
+
+with open('data.json') as f:
+    data = json.load(f)
+
+
+x = data['x']
+y = data['y']
+
+
+print(f"Lunghezza di x: {len(x)}")
+print(f"Lunghezza di y: {len(y)}")
+
+plt.scatter(x, y)
+plt.plot(x, y)
+plt.xlabel('Invio Rate (1/avgSendingTime)')
+plt.ylabel('Response Rate (1/responseTime)')
+plt.title('Grafico delle prestazioni')
+
+plt.show()
