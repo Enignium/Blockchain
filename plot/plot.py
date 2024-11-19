32242967e6ef4997d2f5cc063453f33c71d5ca3f
xAlekos <garufialessandro0@gmail.com>
2024-11-19 14:21:28 +0100

    yeeee

--- plot/plot.py
+++ plot/plot.py
@@ -6,15 +6,17 @@ with open('data.json') as f:
 
 
 x = data['x']
+x_s = [i * 1000.0 for i in x]
 y = data['y']
+y_s = [i / 1000.0 for i in y]
 
 print(f"Lunghezza di x: {len(x)}")
 print(f"Lunghezza di y: {len(y)}")
 
-plt.scatter(x, y)
-plt.plot(x, y)
-plt.xlabel('Invio Rate (1/avgSendingTime)')
-plt.ylabel('Tempo di risposta (ms))')
+plt.scatter(x_s, y_s)
+plt.plot(x_s, y_s)
+plt.xlabel('Rate di invio (req/s)')
+plt.ylabel('Tempo di risposta (s)')
 plt.title('Grafico delle prestazioni')
 
 plt.show()
