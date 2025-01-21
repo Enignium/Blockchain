1b85a30d62a2cde70f81f783b3c5f176bd41e414
xAlekos <garufialessandro0@gmail.com>
2025-01-21 22:25:59 +0100

    bella

--- fs2/interface.py
+++ fs2/interface.py
@@ -1,4 +1,4 @@
-import os
+import os,io
 from web3 import Web3
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 import json
@@ -23,24 +23,22 @@ def get_contract():
     user_address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' ##per ora non fa niente
     user_address = Web3.to_checksum_address(user_address)
 
-                        ##file_names = contract.functions.getFileNames().call()
 
-    return contract
+    return contract , w3
 
 
 
 class FileStorageFUSE(Operations):
 
     def __init__(self):
-        self.contract = get_contract()
+        self.contract,self.w3 = get_contract()
         
-
 
     def getattr(self, path, fh=None):
         print("Getattr" + path)
         if path == '/':
             return {
-                'st_mode': 0o755 | S_IFDIR,  # Directory
+                'st_mode': 0o755 | S_IFDIR,  
                 'st_nlink': 2,
             }
         else:
@@ -53,18 +51,13 @@ class FileStorageFUSE(Operations):
 
             file_content = self.contract.functions.getFile(file_name).call()
             return {
-                'st_mode': 0o444 | S_IFREG,  # File read-only
+                'st_mode': 0o666 | S_IFREG,
                 'st_size': len(file_content),
                 'st_nlink': 1,
             }
 
     def open(self, path, flags):
         print(f"Open chiamata per {path}")
-        ##file_name = path[1:]
-        ##exists = self.contract.functions.isFileAlreadyIn(file_name).call() --In teoria controlla getAttr prima
-        ##if not exists:
-            ##print(f"{path} NON TROVATO")
-            ##raise FuseOSError(ENOENT)
         return 0
 
 
@@ -87,7 +80,47 @@ class FileStorageFUSE(Operations):
         file_content = self.contract.functions.getFile(file_name).call()
 
         return file_content[offset:offset+size]
+    
+    def create(self, path, mode):
+        print("CREATING" + path)
+        buffer = io.BytesIO()
+        buffer.write(b'')
+        file_name = path[1:]
+        tx = self.contract.functions.uploadFile(file_name,b"").transact({
+            'from': self.w3.eth.accounts[0]  
+        })
+        self.w3.eth.wait_for_transaction_receipt(tx)
+
+        print("File creato(?)")
+        return 0
+    
+    def write(self, path, data, offset, fh):
+        file_name = path[1:]
+        file_content = self.contract.functions.getFile(file_name).call()
+        new_content =  file_content[:offset] + data + file_content[offset + len(data):]
+        tx = self.contract.functions.uploadFile(file_name,new_content).transact({
+            'from': self.w3.eth.accounts[0]  
+        });
+        self.w3.eth.wait_for_transaction_receipt(tx)
+
+        print("Scrittura scrittata(?)")
+        return len(data)
+    
+    def truncate(self, path, length, fh=None):
+
+        print("Troncatura" + path)
+
+        file_name = path[1:]
+        file_content = self.contract.functions.getFile(file_name).call()
+        new_content =  file_content[:length]
+    
+        tx = self.contract.functions.uploadFile(file_name,new_content).transact({
+            'from': self.w3.eth.accounts[0]  # L'account da cui inviare la transazione
+        });
+
+        self.w3.eth.wait_for_transaction_receipt(tx)
+        print("Troncatura troncata(?)")
 
 if __name__ == '__main__':
     
-    fuse = FUSE(FileStorageFUSE(), './mnt', foreground=True)
+    fuse = FUSE(FileStorageFUSE(), './mnt', foreground=True,debug=True)
